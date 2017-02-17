ce331ea0a556a79c30f574956dbea6841009b741
Yuri <yuri.manahan@gmail.com>
2017-02-17 15:20:21 -0500

    Displaying name and votes

--- election.js
+++ election.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'JSON' ,
+  }).done(function(responseData){
+    console.log(responseData.candidates[0].name);
+    var test = $('#result');
+    for (i = 0; i < responseData.candidates.length; i++){
+      var can = $('<li>');
+      // can.html(responseData.candidates[i].name);
+      can.html(responseData.candidates[i].name + ' has ' + responseData.candidates[i].votes + ' votes.');
+      test.append(can);
+    }
+
+  });
+
   // Imagination!
 
 });
