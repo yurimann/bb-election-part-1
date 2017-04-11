e194cc9c4cb4b2f92b471a14a3a1641633d1f0bb
yurimann <yuri.manahan@gmail.com>
2017-04-11 15:29:28 -0400

    Create ajax request

--- election.js
+++ election.js
@@ -1,21 +1,38 @@
 $(document).ready(function() {
-
   $.ajax({
     url: 'https://bb-election-api.herokuapp.com/',
     method: 'GET',
     dataType: 'JSON' ,
   }).done(function(responseData){
-    console.log(responseData.candidates[0].name);
     var test = $('#result');
     for (i = 0; i < responseData.candidates.length; i++){
       var can = $('<li>');
       // can.html(responseData.candidates[i].name);
       can.html(responseData.candidates[i].name + ' has ' + responseData.candidates[i].votes + ' votes.');
       test.append(can);
+
+      $('#result').append(
+        '<form method="POST" action="https://bb-election-api.herokuapp.com/vote" class="here">' +
+          '<input type="hidden" name="name" value="'+ responseData.candidates[i].name+'">' +
+          '<input class="vote" type="submit" value="vote"></form>'
+      );
     }
 
+    $('.here').click(function(e){
+
+      e.preventDefault();
+
+      $.ajax({
+        url: 'https://bb-election-api.herokuapp.com/vote',
+        method: 'POST',
+        data: {name: $(this).children('input[type=hidden]').val()},
+        dataType: 'JSON'
+
+      }).done(function(e){
+        console.log(e);
+      }).fail(function(jqXHR, textStatus, errorThrown){
+        console.log(jqXHR);
+      })
+    });
   });
-
-  // Imagination!
-
 });
